e13f6146a475205f20f2665b87b9e3b673ae23ec
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-03-02 03:44:53 +0530

    second commit

---
+++ src/ClassComponent/Sample.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class Sample extends Component{
+    render(){
+        return(
+            <>
+            <h1>Class Component Example</h1>
+            <h2>This is Sample Component</h2>
+            </>
+        )
+    }
+}
---
+++ src/FunctionalComponent/SampleF.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SampleF(){
+    return(
+        <>
+            <h1>Functional Component Example</h1>
+            <h2>This is SampleF Component</h2>
+        </>
+    )
+}
