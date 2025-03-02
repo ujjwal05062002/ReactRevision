04b4d432cc7064abf04827f4815dd4224c2f2482
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-03-02 19:13:49 +0530

    third commit

--- src/ClassComponent/Sample.jsx
+++ src/ClassComponent/Sample.jsx
@@ -1,5 +1,21 @@
 import React, {Component} from "react";
 
+export let name1 = "Ujjwal Upadhyay"
+export let arr1 = [10,20,30,40,50]
+export let emp1  = {
+    id : 1001,
+    name : "Ujjwal Upadhyay",
+    dsg : "Trainee",
+    city : "Meerut",
+    state  : "Uttar Pradesh"
+}
+
+export function display1(){
+    return(
+        <h3>in display1() function of Sample Component</h3>
+    )
+}
+
 export default class Sample extends Component{
     render(){
         return(
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';  //optional line
 import ReactDOM from 'react-dom/client'
-import Sample from './ClassComponent/Sample';
+import Sample, {name1, arr1, emp1, display1} from './ClassComponent/Sample';
 import SampleF from './FunctionalComponent/SampleF';
 
 let root = ReactDOM.createRoot(document.getElementById("root"))
@@ -8,6 +8,14 @@ let root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
     <>
     <Sample />
+    <h3>Name1 : {name1}</h3>
+    <h3>Array1 : {arr1}</h3>
+    <h3>Emp1oyee ID : {emp1.id}</h3>
+    <h3>Emp1oyee Name : {emp1.name}</h3>
+    <h3>Emp1oyee Designation : {emp1.dsg}</h3>
+    <h3>Emp1oyee City : {emp1.city}</h3>
+    <h3>Emp1oyee State : {emp1.state}</h3>
+    {display1()}
     <hr></hr>
     <SampleF />
     </>
