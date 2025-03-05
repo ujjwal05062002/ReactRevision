db08e88d9c769c285b7befb14d39200013179938
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-03-05 18:01:18 +0530

    lifting state up class component

--- src/ClassComponent/SendDataCP/Parent.jsx
+++ src/ClassComponent/SendDataCP/Parent.jsx
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
-import Child from './Child';
+import React, { Component } from "react";
+import Child from "./Child";
 
-export default class Parent extends Component{
-    constructor(){
-        super()
-        this.state = {
-            data : []
-        }     
+export default class Parent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: [],
     }
+  }
 
-    getData(data){
-        this.setState({data:data})
-    }
-    render(){
-        return(
-            <>
-                <h2>Parent Component</h2>
-                <h3>Lifting State up example or sending data from child to parent</h3>
-                {
-                    this.state.data.length?
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Id : </th>
-                                <th>Name : </th>
-                                <th>Designation : </th>
-                                <th>Salary : </th>
-                                <th>City : </th>
-                                <th>State : </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.props.data.map((item,index)=>{
-                                <tr key={index}>
-                                    <th>{item.id} </th>
-                                    <th>{item.name} </th>
-                                    <th>{item.dsg}</th>
-                                    <th>{item.salary} </th>
-                                    <th>{item.city}</th>
-                                    <th>{item.state}</th>
-                                </tr>
-                            })}
-                        </tbody>
-                    </table>
-                    : ""
-                }
-                <hr />
-                <Child  />
-            </>
+  getData = (data) => {
+    this.setState({ data: data });
+  }
+  render() {
+    return (
+      <>
+        <h2>Parent Component</h2>
+        <h3>Lifting State up example or sending data from child to parent</h3>
+        {this.state.data.length ? 
+        (
+          <table border="2px solid black" cellPadding="10px">
+            <thead>
+              <tr>
+                <th>Id  </th>
+                <th>Name  </th>
+                <th>Designation  </th>
+                <th>Salary  </th>
+                <th>City  </th>
+                <th>State  </th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <th>{item.id} </th>
+                    <th>{item.name} </th>
+                    <th>{item.dsg} </th>
+                    <th>{item.salary} </th>
+                    <th>{item.city} </th>
+                    <th>{item.state} </th>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         )
-    }
+         : ("")}
+        <hr />
+        <Child getData={this.getData}/>
+      </>
+    );
+  }
 }
