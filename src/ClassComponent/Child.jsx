00f8064340cf8750ed7bc68b2157c52bc4cd5d9b
ujjwal05062002 <102460339+ujjwal05062002@users.noreply.github.com>
2025-03-20 05:18:53 +0530

    Add files via upload

--- src/ClassComponent/Child.jsx
+++ src/ClassComponent/Child.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 export default class Child extends Component {
+    shouldComponentUpdate(){
+        return false
+    }
   render() {
+    console.log("Child Component is rendered");
+    
     return (
       <div>
         <h2>Child Component</h2>
+
       </div>
     );
   }
--- src/ClassComponent/Parent.jsx
+++ src/ClassComponent/Parent.jsx
@@ -11,12 +11,16 @@ export default class Parent extends Component {
     }
 
   render() {
+
+    console.log("Parent Component is rendered");
+    
     return (
       <div>
         <h1>Should Component Update VS Pure Component</h1>
         <h2>Parent Component</h2>
         <h3>num = {this.state.num}</h3>
         <button onClick={()=>this.setState({num:this.state.num+1})}>increment</button>
+        <button onClick={()=>this.setState({num:this.state.num-1})} >decrement</button>
         <hr />
         <Child />
       </div>
